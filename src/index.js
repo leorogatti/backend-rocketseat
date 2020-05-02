046e016f770dfe956253b242d53823edf653a8b4
Leonardo Rogatti <leorrogatti@gmail.com>
2020-05-02 17:00:45 -0300

    Added status http

--- src/index.js
+++ src/index.js
@@ -23,29 +23,44 @@ app.post('/projects', (request, response) => {
 
   projects.push(project);
 
-  return response.json(project)
-  
+  return response.status(201).json(project)
+
 });
 
 
 app.put('/projects/:id', (request, response) => {
   const { id } = request.params;
+  const { title, owner } = request.body;
   
-  console.log(id)
+  const projectIndex = projects.findIndex(project => project.id === id);
 
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-    'Projeto 3',
-  ])
+  if (projectIndex < 0) {
+    return response.status(404).json({ error: 'Project not found' })
+  }
+
+  const project = {
+    id,
+    title,
+    owner,
+  };
+
+  projects[projectIndex] = project;
+
+  return response.json(project);
 });
 
 app.delete('/projects/:id', (request, response) => {
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-    'Projeto 3',
-  ])
+  const { id } = request.params;  
+
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(404).json({ error: 'Project not found' })
+  }
+
+  projects.splice(projectIndex, 1);
+  
+  return response.status(204).send();
 });
 app.listen(7777), () => {
   console.log('Back-end started! 🚀');
